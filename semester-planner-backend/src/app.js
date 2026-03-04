3d79654255fcf1f7ce1b153da05341104547c91c
Sebas <sebastian_montoya82232@elpoli.edu.co>
2026-03-04 15:42:27 -0500

    Lo basico pa iniciar

---
+++ semester-planner-backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('Backend funcionando 🚀');
+});
+
+module.exports = app;
