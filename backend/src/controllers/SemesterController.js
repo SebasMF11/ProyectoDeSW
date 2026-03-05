38a2c109d11c8a1d94bb5864a8170445678888ed
Sebas <sebastian_montoya82232@elpoli.edu.co>
2026-03-04 23:25:16 -0500

    Primer endpoint

---
+++ backend/src/controllers/SemesterController.js
@@ -0,0 +1,10 @@
+const semesterService = require('../services/SemesterService');
+
+exports.getSemesters = async (req, res) => {
+  try {
+    const semesters = await semesterService.getAll();
+    res.json(semesters);
+  } catch (error) {
+    res.status(500).json({ error: 'Error obteniendo semestres' });
+  }
+};
