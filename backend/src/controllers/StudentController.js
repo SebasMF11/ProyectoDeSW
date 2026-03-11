332c203043beee0c9ad7adcf2ae0b215952b1d5b
Sebas <sebastian_montoya82232@elpoli.edu.co>
2026-03-11 13:44:55 -0500

    Cosas del Auth

--- backend/src/controllers/StudentController.js
+++ backend/src/controllers/StudentController.js
@@ -1,56 +1,44 @@
 const { options } = require('../app');
 const studentService = require('../services/StudentService');
-
 exports.createStudent = async (req, res) => {
 
   try {
 
-    const { name, lastname, email, password, confirmPassword } = req.body;
-
-    if (!name || !lastname || !email || !password || !confirmPassword) {
+    const { name, lastname, email, password, password2 } = req.body;
+    console.log("Datos recibidos:", req.body);
+    
+      console.log(password2 );
+    if (!name || !lastname || !email || !password || !password2) {
       return res.status(400).json({
         error: "Todos los campos son obligatorios"
+        
       });
     }
 
-    if (password !== confirmPassword) {
+    if (password !== password2) {
+      console.log("Error: Las contraseñas no coinciden");
       return res.status(400).json({
         error: "Las contraseñas no coinciden"
       });
     }
 
+const authResult = await studentService.authStudent({
+  name,
+  lastname,
+  email,
+  password
+});
 
+const userId = authResult.user.id;
 
+const student = await studentService.createStudent({
+  idstudent: userId,
+  name,
+  lastname,
+  email
+});
 
-
-/*
-
-    const { data, error } = await supabase.auth.signUp({
-      email: '',
-      password: '',
-      options: {
-      data: {
-        studentname: '',
-        lastname: '',
-      }
-    }
-    } 
-);
- */
-
-
-
-
-
-
-    const student = await studentService.createStudent({
-      name,
-      lastname,
-      email,
-      password
-    });
-
-    res.status(201).json(student);
+  res.status(201).json(student);
 
   } catch (error) {
   console.error(error);
