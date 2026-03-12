0d539556fc5ed9f9fceb03edc83d0a403e640873
Sebas <sebastian_montoya82232@elpoli.edu.co>
2026-03-11 21:10:17 -0500

    Avances pa que funcione el Auth en el Front

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API = "http://localhost:3000/";
 
-export const registerRequest = (student: any) =>
-  axios.post(`${API}student/create`, student);
+export const authRequest = (student: any) =>
+  axios.post(`${API}student/auth`, student);
   
+export const registerRequest = (student: any) =>
+  axios.post(`${API}student/register`, student);
