f1d8afcf823254c6aba95377e1ba7803593a5ad0
sebastian tunjuelo <yotuyotu411@gmail.com>
2026-03-19 12:07:45 -0500

    add big calendar

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import { supabase } from "../integrations/supabase";
 import { KEY_STORAGE } from "../const/constants";
 import { useNavigate } from "react-router";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 function Home() {
+  const localizer = momentLocalizer(moment);
   const navigate = useNavigate();
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
@@ -10,10 +14,23 @@ function Home() {
     localStorage.removeItem(KEY_STORAGE);
     navigate("/auth");
   };
+  const handleSession = async () => {
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+    console.log(session);
+  };
 
   return (
     <div>
+      <button onClick={handleSession}>Ver Session</button>
       <button onClick={handleLogout}>Cerrar Sesión</button>
+      <Calendar
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 }
