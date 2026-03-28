48e02a175409429a15b80eaa99350d84b390684c
saraoyo <sara_echeverri82232@elpoli.edu.co>
2026-03-28 00:03:40 -0500

    Comienzo del calendario

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,3 +1,6 @@
+import { supabase } from "../integrations/supabase";
+import { KEY_STORAGE } from "../const/constants";
+import { useNavigate } from "react-router";
 import logo from "../assets/logo2.png";
 import { MdMenu } from "react-icons/md";
 import { FiUser } from "react-icons/fi";
@@ -5,6 +8,15 @@ import { IoMdSettings } from "react-icons/io";
 import { IoIosLogOut } from "react-icons/io";
 
 function navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+    localStorage.removeItem(KEY_STORAGE);
+    navigate("/auth");
+  };
+
   return (
     <div className="w-full flex items-center justify-between bg-[#009853] p-2">
       <div className="flex items-center gap-2">
@@ -13,11 +25,13 @@ function navbar() {
         <p className="text-[25px] font-bold font-sans text-white">PoliPlan</p>
       </div>
 
-      <div className="flex items-center px-4 gap-3">
-        <FiUser size={30} color="white" />
-        <p className="text-white">Usuario</p>
+      <div className="flex items-center px-10 gap-3">
+        <div className="flex items-center px-20 gap-3">
+          <FiUser size={30} color="white" />
+          <p className="text-white">User</p>
+        </div>
         <IoMdSettings size={30} color="white" />
-        <IoIosLogOut size={30} color="white" />
+        <IoIosLogOut onClick={handleLogout} size={35} color="white" />
       </div>
     </div>
   );
