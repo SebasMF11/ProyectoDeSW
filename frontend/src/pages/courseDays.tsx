54289567f1a0415f6e75f5b67779ab5d5dc15732
sebastian tunjuelo <yotuyotu411@gmail.com>
2026-03-30 18:40:13 -0500

    base form

--- frontend/src/pages/courseDays.tsx
+++ frontend/src/pages/courseDays.tsx
@@ -1,7 +1,43 @@
-import React from "react";
-
-function courseDays() {
-  return <div>courseDays</div>;
-}
-
+import { useNavigate } from "react-router";
+import { useForm } from "react-hook-form";
+import { authRequest } from "../api/auth";
+const courseDays = () => {
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = handleSubmit(async (values) => {
+    const res = await authRequest(values);
+    console.log(res);
+    navigate("/");
+  });
+  return (
+    <div>
+      <div>
+        <p>Crear cuenta</p>
+        <form onSubmit={onSubmit}>
+          <input
+            placeholder="Nombre Completo"
+            type="text"
+            {...register("name", { required: true })}
+          />
+          <input
+            placeholder="Correo"
+            type="email"
+            {...register("email", { required: true })}
+          />
+          <input
+            placeholder="Contraseña"
+            type="password"
+            {...register("password", { required: true })}
+          />
+          <input
+            placeholder="Confirmar Contraseña"
+            type="password"
+            {...register("password2", { required: true })}
+          />
+          <button type="submit">Registrarse</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default courseDays;
