681faf1908b117829d0e4af4946c3f3d2c7b11b0
saraoyo <sara_echeverri82232@elpoli.edu.co>
2026-04-13 21:00:57 -0500

    Calendario funcionando

--- frontend/src/hooks/useAssessments.ts
+++ frontend/src/hooks/useAssessments.ts
@@ -56,7 +56,7 @@ export function useAssessments(
           });
         }
 
-        setAssessments({});
+        setAssessments(assessmentsByDate);
       } finally {
         setIsLoading(false);
       }
